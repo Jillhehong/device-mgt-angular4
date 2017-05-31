c3ab0936952774f04a3067f8ce7a0519704f30e4
HongHe <jillhehong@gmail.com>
2017-05-30 22:17:40 -0400

    add index.html

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // render index.html at the starting url
 
 // Catch all other routes and return the index file
-//app.get('*', (req, res) => {
-  //  res.sendFile(path.join(__dirname, 'public/index.html'));
-//});
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 //get data using pg-promise module
 function pgGetSqlUtilityFunc(url, sql) {
